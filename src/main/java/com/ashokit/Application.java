7ad88ceebbf37a59ed2acc2965f9004c5db1543d
manuj4189@gmail.com <manuj4189@gmail.com>
2023-09-15 18:07:34 +0530

    HIS-134 fix provided

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -9,7 +9,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
-	    run.close();
+	    
+		//changes for HIS-134
+		run.close();
+	    
 	}
 
 }
